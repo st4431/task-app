ea07cc180d62d36e17950a01dfd6ee5526dbf407
st <st20020815@icloud.com>
2025-07-24 12:18:29 +0900

    Spring Data JPAに移行するため、Taskクラスに必要なアノテーションを記述。

--- src/main/java/com/takahata/task_app/entity/Task.java
+++ src/main/java/com/takahata/task_app/entity/Task.java
@@ -1,16 +1,28 @@
 package com.takahata.task_app.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Generated;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
+@Entity //このクラスがDBのエンティティ（テーブル）に対応していることを示す
+@Table(name = "task") //対応するテーブル名を指定
 public class Task {
+
+
+    @Id //このフィールドが主キーであることを示す
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DBの自動連番機能をフィールドで使用することを示す
     private int id;
+
     private String title;
     private String description;
+
+    @Enumerated(EnumType.STRING) //Enum型を文字列としてDBに保存する
     private TaskStatus taskStatus;
+
     private LocalDate dueDate;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
