c238cba1e5183f08a0eab92156ce11aa14f6e0e6
st <st20020815@icloud.com>
2025-08-03 10:37:25 +0900

    Postmanによる手動テスト完了。

---
+++ src/test/java/com/takahata/task_app/controller/TaskApiControllerTest.java
@@ -0,0 +1,9 @@
+package com.takahata.task_app.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TaskApiControllerTest {
+
+}
