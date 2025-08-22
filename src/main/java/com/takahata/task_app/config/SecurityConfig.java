b47bf786005e7a3b40b0c01eb72fe2f5acc764c1
st <st20020815@icloud.com>
2025-08-22 17:04:29 +0900

    build.gradleにSpringSecurityの依存関係を追加し、SecurityConfigクラスを作成（途中）。

---
+++ src/main/java/com/takahata/task_app/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.takahata.task_app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain
+}
