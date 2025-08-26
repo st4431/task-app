5bdc03a05e5c3f7d45fbba7b2f94e7800b0cc2c4
st <st20020815@icloud.com>
2025-08-26 09:40:00 +0900

    feature:Dockerファイルを作成

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# GradleとJava17の機能を借りてきて、それをbuilderと名付ける
+FROM gradle:8.5.0-jdk17-alpine AS builder
+
+# /appという名前の作業スペースを用意する
+WORKDIR /app
+
+# 自分のSpringBootの設計図や設定ファイルをコピーする
+# 「あなたのPCになるbuild.gradleとsettings.gradleという2つの設定ファイルを、コンテナ内の/appという場所にコピーしてください。」
+# ソースコードに必要なライブラリをダウンロードする
+COPY build.gradle settings.gradle ./
+COPY gradle ./gradle
+RUN gradle dependencies
+
+# 次に、アプリケーションのソースコードを全てコピーします。
+COPY src ./src
+
+# ビルドを行う
+RUN gradle build --no-daemon
+
+# Java17のアプリケーションを実行するためだけの、最小限の道具（JRE）が入った、小さいコンテナを持ってくる
+FROM openjdk:17-jre-slim
+
+# /appという名前の作業スペースを用意する
+WORKDIR /app
+
+# /appにapp.jarという名前で.jarファイルをコピーする
+# コピー元は最初にbuilderと名付けた部分
+COPY --from=builder /app/build/libs/*.jar app.jar
+
+# コンテナを起動するときに必ず実行して欲しい命令文を指定する
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/takahata/task_app/config/SecurityConfig.java
+++ src/main/java/com/takahata/task_app/config/SecurityConfig.java
@@ -1,10 +1,29 @@
 package com.takahata.task_app.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(login -> login
+                .loginProcessingUrl("/login")//ログイン画面のurl
+                .loginPage("/login")
+                .defaultSuccessUrl("/")//ログインが完了した後に遷移させるべき場所を示したurl
+                .failureUrl("/login?error")//ログイン失敗した時に遷移させるべき場所を示したurl
+                .permitAll()//
+        ).logout(logout -> logout
+                .logoutSuccessUrl("/")//ログアウトした後の遷移先
+        ).authorizeHttpRequests(ahr -> ahr
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/admin").hasRole("ADMIN")//「/admin」というurlは、ADMIN（管理者）でないと遷移できませんということ
+                .anyRequest().authenticated()//
+                );
+        return http.build();
+    }
 }
