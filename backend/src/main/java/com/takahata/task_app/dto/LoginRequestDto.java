b78ab1bebddad09b8dd4c55389a5972ec76d3c8d
st <st20020815@icloud.com>
2025-09-18 21:34:25 +0900

    feat:各RequestDtoに@NotBlankを追記

--- backend/src/main/java/com/takahata/task_app/dto/LoginRequestDto.java
+++ backend/src/main/java/com/takahata/task_app/dto/LoginRequestDto.java
@@ -1,8 +1,11 @@
 package com.takahata.task_app.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+// TODO:詳細なバリデーションを追加（エラーメッセージも含む）
 @Data
+@NotBlank
 public class LoginRequestDto {
     private String username;
     private String password;
