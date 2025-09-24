6043b42eecdc4ee3ee7efdf01c7d6b0813794d4a
st <st20020815@icloud.com>
2025-09-24 12:30:23 +0900

    feat:JWT認証実装のため、AuthControllerのautheticateUserを作成し、LoginResponseDtoのフィールドを文字列のjwtのみに変更し、コンストラクタでセットするようにした

--- backend/src/main/java/com/takahata/task_app/controller/AuthController.java
+++ backend/src/main/java/com/takahata/task_app/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.takahata.task_app.controller;
 
+import com.takahata.task_app.config.JwtUtils;
 import com.takahata.task_app.mapper.UserMapper;
 import com.takahata.task_app.dto.LoginRequestDto;
 import com.takahata.task_app.dto.LoginResponseDto;
@@ -12,18 +13,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    private final UserService userService;
+    private final JwtUtils jwtUtils;
     private final UserMapper userMapper;
+    private final UserService userService;
+    private final UserDetailsService userDetailsService;
     private final AuthenticationManager authenticationManager;
 
     // ResponseEntityを使用すると、ステータスコードとレスポンスボディ（JSONの戻り値みたいなもの）を自由に設定できる
@@ -36,16 +43,21 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> authenticateUser(@RequestBody LoginRequestDto loginRequestDto) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginRequestDto.getUsername(),
-                        loginRequestDto.getPassword()
-                )
-        );
-        LoginResponseDto loginResponseDto = new LoginResponseDto();
+    public ResponseEntity<LoginResponseDto> authenticateUser(@RequestBody LoginRequestDto loginRequestDto) {
 
-        return ResponseEntity.ok("");
+        // ユーザー名、パスワードの認証
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequestDto.getUsername(), loginRequestDto.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid username or password.", e);
+        }
+
+        // ユーザー名からJWTを取得し、クライアントに返す
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequestDto.getUsername());
+        final String jwt = jwtUtils.generateToken(userDetails);
+        return ResponseEntity.ok(new LoginResponseDto(jwt));
     }
 
 }
--- backend/src/main/java/com/takahata/task_app/dto/LoginResponseDto.java
+++ backend/src/main/java/com/takahata/task_app/dto/LoginResponseDto.java
@@ -1,10 +1,15 @@
 package com.takahata.task_app.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+@Getter
+// 引数なしのデフォルトコンストラクタを自動生成する
+@NoArgsConstructor
+// すべてのフィールドを引数に持つコンストラクタを自動生成する
+@AllArgsConstructor
 public class LoginResponseDto {
-    private long id;
-    private String username;
-    private String role;
+    private String jwt;
 }
