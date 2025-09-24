61d77f94a0dc2df5c2e7d6a54875ce067442d713
st <st20020815@icloud.com>
2025-09-24 11:36:30 +0900

    feat:JWT認証実装のため、SecurityConfigを編集

--- backend/src/main/java/com/takahata/task_app/config/SecurityConfig.java
+++ backend/src/main/java/com/takahata/task_app/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.takahata.task_app.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,13 +8,18 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private JwtAuthFilter jwtAuthFilter;
 
     // formLoginだとSpring Securityのみで認証を処理してしまい、Controller層にリクエストが届かないため、Rest APIには対応していない
     // SSRのみに対応している
@@ -21,18 +27,21 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .formLogin(login -> login
-                        .loginPage("/api/auth/login")
-                        .loginProcessingUrl("/api/auth/login")
-                        .defaultSuccessUrl("/", true)
-                        .permitAll()
-                )
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/auth/register").permitAll()
+                // REST APIではセッションは不要なので、無効化する
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
+                )
 
+                // JwtAuthFilter をSpring Securityのフィルターチェーンに追加
+                // UsernamePasswordAuthenticationFilter の前に実行することで、
+                // ID、パスワード認証の前にJWTの検証が行われるようになる
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
 
-                );
+                // CSRF攻撃の対策を無効化
+                // JWTを使用する場合、セッションを使わないため、CSRF対策は不要となるため
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
